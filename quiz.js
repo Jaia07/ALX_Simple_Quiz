62e73b0d6a46d9ab79b03db338f9c15084d8e802
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-01-19 21:34:30 +0100

    Corrected interactive quiz javascript file

--- quiz.js
+++ quiz.js
@@ -1,20 +1,20 @@
-
 function checkAnswer() {
-    let correctAnswer = 4;
-}
-
-let userAnswer = document.querySelector("#choice1").value;
-console.log(userAnswer);
-
-
-let feedback = document.getElementById("feedback");
-
-if(userAnswer == correctAnswer){
-    feedback.textContent = "Correct! Well done.";
-}else{
-    feedback.textContent = "That's incorrect. Try again!";
-}
-
-let submitAnswer = document.getElementById("submit-answer");
-
-submitAnswer.addEventListener('click', checkAnswer);
+    // Identify the Correct Answer
+    const correctAnswer = "4"; 
+  
+    // Retrieve the User's Answer
+    const checkedRadio = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer = checkedRadio.value;
+  
+    // Compare the User's Answer with the Correct Answer
+    const feedbackElement = document.getElementById("feedback"); 
+    if(userAnswer === correctAnswer) {
+      feedbackElement.textContent = "Correct! Well done.";
+    }else {
+      feedbackElement.textContent = "That's incorrect. Try again!";
+    }
+  }
+  
+  // Add an Event Listener to the Submit Button
+  const submitButton = document.getElementById("submit-answer");
+  submitButton.addEventListener("click", checkAnswer); 
